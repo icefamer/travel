928ca5bc8b7e4bf945926fc14aca4cbe1fcc950a
icefamer <icefamer@gmail.com>
2018-12-23 13:10:13 +0800

    '首页header制作'

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="header">
-        <div class="header-left">返回</div>
-        <div class="header-input">输入城市/景点/游玩主题</div>
-        <div class="header-right">城市</div>
+        <div class="header-left">
+          <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+          <span class="iconfont">&#xe632;</span>
+          输入城市/景点/游玩主题
+        </div>
+        <div class="header-right">
+          城市
+          <span class="iconfont arrow-icon">&#xe62d;</span>
+        </div>
     </div>
 </template>
 
@@ -13,19 +21,26 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '~styles/varibles.styl';
   .header
     display flex
     line-height .86rem
-    background-color #00bcd4
+    background-color $bgColor
     color #ffffff
     .header-left
       width .64rem
       float left
+      .back-icon
+        text-align center
+        font-size .4rem
     .header-right
       width 1.24rem
       float right
       text-align center
+      .arrow-icon
+        font-size .24rem
     .header-input
+      padding-left .2rem
       line-height .64rem
       height .64rem
       margin-top .12rem
