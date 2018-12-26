6295637593bf49c3b6c6ec97d08aad9c7f784fa9
icefamer <icefamer@gmail.com>
2018-12-26 16:36:21 +0800

    城市选择输入框制作

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height .72rem
+    background-color $bgColor
+    padding 0 .1rem
+    .search-input
+      box-sizing border-box
+      height .62rem
+      padding 0 .1rem
+      line-height .62rem
+      width 100%
+      text-align center
+      border-radius .06rem
+      color #666666
+</style>
